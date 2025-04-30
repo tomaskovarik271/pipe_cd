0fefe312c52a8d3782e52868e7cc66e644aeb44e
tomaskovarik271 <64414613+tomaskovarik271@users.noreply.github.com>
2025-04-30 12:01:58 +0200

    Build: Re-introduce AuthProvider import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,16 +5,16 @@ import './index.css'
 // import { ApolloProvider } from '@apollo/client' // Comment out for build test
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 // import { client } from '@/apollo-client.ts' // Comment out for build test
-// import { AuthProvider } from '@/context/AuthContext' // Comment out for build test
+import { AuthProvider } from '@/context/AuthContext' // Uncomment AuthProvider
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <AuthProvider> */}{/* Comment out for build test */}
+    <AuthProvider> {/* Uncomment AuthProvider */}
       {/* <ApolloProvider client={client}> */}{/* Comment out for build test */}
         <ChakraProvider value={defaultSystem}>
           <App />
         </ChakraProvider>
       {/* </ApolloProvider> */}{/* Comment out for build test */}
-    {/* </AuthProvider> */}{/* Comment out for build test */}
+    </AuthProvider> {/* Uncomment AuthProvider */}
   </React.StrictMode>,
 )
