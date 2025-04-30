c4424395c4782602705dfe6db21f33eb222ed9b2
tomaskovarik271 <64414613+tomaskovarik271@users.noreply.github.com>
2025-04-30 12:22:42 +0200

    Fix: Add explicit extension to AuthProvider import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 // import { ApolloProvider } from '@apollo/client' // Comment out for build test
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 // import { client } from '@/apollo-client.ts' // Comment out for build test
-import { AuthProvider } from '@/context/AuthContext' // Uncomment AuthProvider
+import { AuthProvider } from '@/context/AuthContext.tsx' // Explicitly add .tsx extension
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
