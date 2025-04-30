2b01c039888611423b78bf42b6d02e2a31feea97
tomaskovarik271 <64414613+tomaskovarik271@users.noreply.github.com>
2025-04-30 13:11:22 +0200

    Build: Add dummy import for isolation test

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,16 +5,20 @@ import './index.css'
 // import { ApolloProvider } from '@apollo/client' // Comment out for build test
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 // import { client } from '@/apollo-client.ts' // Comment out for build test
-import { AuthProvider } from '@/context/AuthContext.tsx' // Explicitly add .tsx extension
+// import { AuthProvider } from '@/context/AuthContext.tsx' // Comment out for build test
+import { DUMMY_MESSAGE } from '@/dummy.ts'; // Import from new dummy file using alias
+
+// Log the dummy message to ensure it's used and not tree-shaken too early
+console.log(DUMMY_MESSAGE);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider> {/* Uncomment AuthProvider */}
+    {/* <AuthProvider> */}{/* Comment out for build test */}
       {/* <ApolloProvider client={client}> */}{/* Comment out for build test */}
         <ChakraProvider value={defaultSystem}>
           <App />
         </ChakraProvider>
       {/* </ApolloProvider> */}{/* Comment out for build test */}
-    </AuthProvider> {/* Uncomment AuthProvider */}
+    {/* </AuthProvider> */}{/* Comment out for build test */}
   </React.StrictMode>,
 )
