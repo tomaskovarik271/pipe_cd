0c7899a4b714bb671845d1928c3cb31a2de0b14e
tomaskovarik271 <64414613+tomaskovarik271@users.noreply.github.com>
2025-05-01 08:23:41 +0200

    Build: Attempt to restore AuthProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 // import { ApolloProvider } from '@apollo/client' // Temporarily comment out
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 // import { client } from '@/apollo-client.ts' // Temporarily comment out
-// import { AuthProvider } from '@/context/AuthContext.tsx' // Temporarily comment out
+import { AuthProvider } from '@/context/AuthContext.tsx' // Restore AuthProvider
 // import { supabase } from '@/supabase-client.ts'; // Remove direct supabase import
 // import { useAuth } from '@/hooks/useAuth.tsx'; // Remove direct useAuth import
 
@@ -18,13 +18,13 @@ import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <AuthProvider> */}{/* Temporarily comment out */}
+    <AuthProvider> {/* Restore AuthProvider */}
       {/* <ApolloProvider client={client}> */}{/* Temporarily comment out */}
         <ChakraProvider value={defaultSystem}>
           {/* <AuthTester /> */}{/* Remove tester component */}
           <App />
         </ChakraProvider>
       {/* </ApolloProvider> */}{/* Temporarily comment out */}
-    {/* </AuthProvider> */}{/* Temporarily comment out */}
+    </AuthProvider> {/* Restore AuthProvider */}
   </React.StrictMode>,
 )
