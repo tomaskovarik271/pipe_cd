0de129597571c770160e6ff8929bb79b70abfdbe
tomaskovarik271 <64414613+tomaskovarik271@users.noreply.github.com>
2025-05-01 08:26:57 +0200

    Build: Re-apply workaround commenting out providers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import { ApolloProvider } from '@apollo/client' // Temporarily comment out
+// import { ApolloProvider } from '@apollo/client' // Keep commented out
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
-// import { client } from '@/apollo-client.ts' // Temporarily comment out
-import { AuthProvider } from '@/context/AuthContext.tsx' // Restore AuthProvider
+// import { client } from '@/apollo-client.ts' // Keep commented out
+// import { AuthProvider } from '@/context/AuthContext.tsx' // Re-comment AuthProvider
 // import { supabase } from '@/supabase-client.ts'; // Remove direct supabase import
 // import { useAuth } from '@/hooks/useAuth.tsx'; // Remove direct useAuth import
 
@@ -18,13 +18,13 @@ import { AuthProvider } from '@/context/AuthContext.tsx' // Restore AuthProvider
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider> {/* Restore AuthProvider */}
-      {/* <ApolloProvider client={client}> */}{/* Temporarily comment out */}
+    {/* <AuthProvider> */}{/* Re-comment AuthProvider */}
+      {/* <ApolloProvider client={client}> */}{/* Keep commented out */}
         <ChakraProvider value={defaultSystem}>
           {/* <AuthTester /> */}{/* Remove tester component */}
           <App />
         </ChakraProvider>
-      {/* </ApolloProvider> */}{/* Temporarily comment out */}
-    </AuthProvider> {/* Restore AuthProvider */}
+      {/* </ApolloProvider> */}{/* Keep commented out */}
+    {/* </AuthProvider> */}{/* Re-comment AuthProvider */}
   </React.StrictMode>,
 )
